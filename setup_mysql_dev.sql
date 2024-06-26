2359f23f37839e3dba311100f842f827d1611b1d
EuniceGeorge <georgeunice408.com>
2024-06-26 16:23:24 +0100

    task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,5 +7,5 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- set privileges for the user --
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 -- set SELECT privileges for user hbnb_dev on performance_schema --
-GRANT SELECT ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,5 +5,5 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- set privileges for the user --
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- set SELECT privileges for user hbnb_test on performance_schema --
-GRANT SELECT ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
