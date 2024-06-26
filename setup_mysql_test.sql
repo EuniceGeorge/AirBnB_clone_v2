20324e8d8ffe63ba768b35ec9637f103ab09f50d
EuniceGeorge <georgeunice408.com>
2024-06-26 16:37:13 +0100

    task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,6 +4,5 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- set privileges for the user --
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
--- set SELECT privileges for user hbnb_test on performance_schema --
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
