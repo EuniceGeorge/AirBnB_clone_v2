98332b283673d008210649a8715f6f143d4b7b65
EuniceGeorge <georgeunice408.com>
2024-07-31 14:49:23 +0100

    Task

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -22,6 +22,7 @@ def show_c_text(text):
     text = text.replace('_', ' ')
     return "C {}".format(text)
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def show_python_text(text="is cool"):
