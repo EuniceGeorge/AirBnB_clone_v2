c9b5a366b2088f466554d643de0821583b27b97e
EuniceGeorge <georgeunice408.com>
2024-07-31 13:38:11 +0100

    web_flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" script that starts a Flask web application
+"""
+script that starts a Flask web application:
 """
 from flask import Flask
 
@@ -8,6 +9,9 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
